308c0091e99377222c4b2293888975ada49bb5d3
iwane, matthew <mattiwane@gmail.com>
2023-10-16 13:50:52 -0400

    nav bar template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css' */
-// import Heading from "./components/Heading"
+import Heading from "./components/Heading"
 import Resources from "./components/Resources"
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Heading title={"Hi"}/> */}
+      <Heading/>
       <Resources/>
     </>
   )
