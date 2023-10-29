d5edde4d18de9912dd34cb00cd0c3ed5896c8ffc
Matthew T. Iwane <mattiwane@gmail.com>
2023-10-29 16:43:11 -0400

    starting home and resources page

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import "./module.shared.css"
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-section'>
+      <div className='logo'>
+        {/* <img src="../../images/logo.jpg" alt="logo" /> */}
+        <h1>Book Arts Catalog</h1>
+      </div>
+      <div>
+        <div className='pfp'></div>
+      </div>
+    </div>
   )
 }
