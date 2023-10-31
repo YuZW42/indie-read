520f4b3a94fb02c5573177a7b6be40a3236c7706
Yu Zeng <zengy2003@gmail.com>
2023-10-30 20:39:40 -0400

    Fixes after the merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 
 import { Home } from "./pages/Home.tsx"
-import { Resources } from "./pages/Resources.tsx"
+import { Resource } from "./pages/Resources.tsx"
 
 function App() {
   return (
     <>
       <Routes>                
         <Route path='/' element={<Home/>}/>     
-        <Route path='/resources' element={<Resources/>}/>                   
+        <Route path='/resources' element={<Resource/>}/>                   
       </Routes>
     </>
   )
