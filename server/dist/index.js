56322554799883ce47e40b4caee13bf37a5af18d
Yu Zeng <zengy2003@gmail.com>
2023-12-08 19:20:35 -0500

    npm build

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,12 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
+const passport_1 = __importDefault(require("passport"));
+const prismaclient_1 = __importDefault(require("./pages/shared/prismaclient"));
 const resources_1 = __importDefault(require("./pages/resource/resources"));
 const searchquery_1 = __importDefault(require("./pages/search/searchquery"));
-const cors = require('cors');
+const google_1 = __importDefault(require("./pages/login/google"));
+const save_fav_1 = __importDefault(require("./pages/save_fav/save_fav"));
+const display_fav_1 = __importDefault(require("./pages/save_fav/display_fav"));
 const app = (0, express_1.default)();
 const port = 5002;
-app.use(cors());
+app.use((0, cookie_session_1.default)({
+    name: 'session',
+    keys: ['indie-book'],
+    maxAge: 24 * 60 * 60 * 100
+}));
+app.use(passport_1.default.initialize());
+app.use(passport_1.default.session());
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
 app.get('/', (req, res) => {
     res.send('Hello');
 });
@@ -28,11 +45,57 @@ app.get('/search_keyword', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching books' });
     }
 });
+app.get('/save_fav', async (req, res) => {
+    const { id, bookId } = req.query;
+    console.log(id, bookId);
+    try {
+        const result = await (0, save_fav_1.default)(String(id), Number(bookId));
+        res.status(200).json(result);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+app.get('/get_fav', async (req, res) => {
+    const preference = req.query;
+    try {
+        const result = await (0, display_fav_1.default)(preference);
+        res.status(200).json(result);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+// Add this route to retrieve user data by ID
+app.get('/user', async (req, res) => {
+    try {
+        const userEmail = req.query.list; // Extract the email from the query params
+        if (!userEmail) {
+            return res.status(400).json({ message: 'Email parameter is missing' });
+        }
+        // Retrieve the user data based on the user's email
+        const user = await prismaclient_1.default.user.findFirst({
+            where: { email: userEmail },
+            // Include additional fields if needed
+            include: { /* additional fields */}
+        });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        console.log(user);
+        return res.status(200).json(user);
+    }
+    catch (error) {
+        console.error('Error retrieving user:', error);
+        return res.status(500).json({ message: 'Error retrieving user data' });
+    }
+});
 app.get('/api/healthchecker', (_, res) => {
     res.status(200).json({
         status: 'success',
-        message: 'server running',
+        message: 'server running'
     });
 });
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.use('/auth', google_1.default);
+app.listen(port, () => console.log(`Server running on port ${port}`));
 //# sourceMappingURL=index.js.map
--- server/src/database/user.ts
+++ server/src/database/user.ts
@@ -1,23 +1,3 @@
 import prisma from '../pages/shared/prismaclient'
 //{log:["query"]}
 
-export async function get_user () {
-  //await prisma.user.deleteMany()
-  const user = await prisma.book.create({
-  data:{"title":"gt",
-  "author":"jm",
-  "price":4,
-  "dimensions":["1","2"],
-  "materials":"1",
-  "description":"2",
-  
-
-
-}
-  },)
-  console.log(user)
-  return user;
-  
-}
-
-export default get_user 
