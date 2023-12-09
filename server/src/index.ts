4d8146b36c40582364f66590d5fcf78c0358eff5
Yu Zeng <zengy2003@gmail.com>
2023-12-08 20:31:39 -0500

    true

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session())
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'indie-read-tnr3.vercel.app',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 }));
