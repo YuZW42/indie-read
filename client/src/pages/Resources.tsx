49acce585d9f325a2f49af84433ebed928982ba2
Yu Zeng <zengy2003@gmail.com>
2023-12-11 12:46:41 -0500

    removed nav bar for res

--- client/src/pages/Resources.tsx
+++ client/src/pages/Resources.tsx
@@ -1,11 +1,9 @@
 import ResourceList from "../components/resources/Resources"
-import NavBar from "../components/shared/NavBar"
 import { Header } from "../components/shared/Header"
 export const Resource = () => {
   return (
     <>
     <Header/>
-      <NavBar/>
       <ResourceList/>
     </>
   )
